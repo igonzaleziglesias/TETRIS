259f2df8b51df903825e9e8470c8a87b05785660
Usuario <Usuario@DESKTOP-P9PU196>
2019-05-28 17:31:59 +0200

    TETRIS 7.0

--- tetris/Interfaz.java
+++ tetris/Interfaz.java
@@ -1,47 +1,90 @@
 package tetris;
 
-import java.awt.*;
+import java.awt.Color;
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
 
 public class Interfaz extends JPanel {
 
     public Interfaz(App juego) {
-        setLayout(null);
-        setVisible(true);
+
         setBounds(250, 0, 250, 600);
-        setBackground(Color.red);
+        JLabel nombre = new JLabel();
+        nombre.setForeground(Color.WHITE);
+        nombre.setText("TETRIS");
+
+        JLabel puntuaciones = new JLabel();
+        puntuaciones.setForeground(Color.WHITE);
+        puntuaciones.setBackground(Color.BLACK);
+        puntuaciones.setText("MEJORES PUNTUACIONES");
+
+        JLabel instrucciones = new JLabel();
+        instrucciones.setForeground(Color.WHITE);
+        instrucciones.setText("Instrucciones:");
+
+        JLabel left = new JLabel();
+        left.setForeground(Color.WHITE);
+        left.setText("VK_LEFT----Mover a la izquierda");
+
+        JLabel right = new JLabel();
+        right.setForeground(Color.WHITE);
+        right.setText("VK_RIGHT----Mover a la derecha");
+
+        JLabel up = new JLabel();
+        up.setForeground(Color.WHITE);
+        up.setText("VK_UP--------------------Girar pieza");
+
+        JLabel down = new JLabel();
+        down.setForeground(Color.WHITE);
+        down.setText("VK_DOWN-------------Bajar rapido");
+
+        JLabel p = new JLabel();
+        p.setForeground(Color.WHITE);
+        p.setText("p-----------------------------------Pausa");
+
+        JLabel espacio = new JLabel();
+        espacio.setForeground(Color.WHITE);
+        espacio.setText("VK_SPACE----------bajar de golpe");
+
+//                + "VK_LEFT----Mover a la izquierda"
+//                + "VK_RIGHT---Mover a la derecha"
+//                + "VK_UP------Girar pieza"
+//                + "VK_DOWN----Bajar rapido"
+//                + "p----------Pausa"
+//                + "VK_SPACE---bajar de golpe");
+//        
+        Object[][] filas = {{"Iago", "10"}};
+        Object[] columnas = {"NICK", "PUNTUACIÓN"};
+
+        JTable tabla = new JTable(filas, columnas);
+        tabla.setBackground(Color.BLACK);
+        tabla.setForeground(Color.WHITE);
+
+        JScrollPane barra = new JScrollPane(tabla);
+        barra.setForeground(Color.WHITE);
+        barra.setBackground(Color.BLACK);
+        add(nombre);
+        add(puntuaciones);
+        add(instrucciones);
+        add(left);
+        add(right);
+        add(up);
+        add(down);
+        add(p);
+        add(espacio);
+        add(barra);
+
+        nombre.setBounds(100, 50, 50, 20);
+        puntuaciones.setBounds(50, 380, 200, 20);
+        barra.setBounds(25, 400, 200, 100);
 
         
+        instrucciones.setBounds(30, 120, 200, 100);
+        left.setBounds(30, 140, 200, 100);
+        right.setBounds(30, 160, 200, 100);
+        up.setBounds(30, 180, 200, 100);
+        down.setBounds(30, 200, 200, 100);
+        p.setBounds(30, 220, 200, 100);
+        espacio.setBounds(30, 240, 200, 100);
         
-        JLabel nombre = new JLabel();
-        nombre.setText("TETRIS");     
-        nombre.setBounds(300, 400, WIDTH, HEIGHT);
-        add(nombre);
-       
-        
-        
-        
-//        
-//        JTable tabla = new JTable();       
-//        tabla.setModel(new javax.swing.table.DefaultTableModel(
-//            new Object [][] {
-//
-//            },
-//            new String [] {
-//                "nick","puntuacion"
-//            }
-//        ));
-//        JScrollPane barrita = new javax.swing.JScrollPane();
-//        barrita.setViewportView(tabla);
-//        tabla.setBounds(300, 400, 200, 400);
-//        
-//        DefaultTableModel modelo = (DefaultTableModel) tabla.getModel();
-//        modelo.addRow(new Object[]{"iago","4"}); 
-//        modelo.addRow(new Object[]{"cesar","5"});      
-//        modelo.addRow(new Object[]{"iago","4"});      
-//        tabla.setModel(modelo);
-//        
-//        add(tabla);
     }
 }
--- tetris/Ventana.java
+++ tetris/Ventana.java
@@ -12,7 +12,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
