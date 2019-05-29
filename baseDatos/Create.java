ba30aa8a3cfb23b41499ee86483dff541001a1d5
igonzaleziglesias <igonzaleziglesias@nautilus5.danielcastelao.org>
2019-05-29 11:07:49 +0200

    Tetris 11.0

---
+++ baseDatos/Create.java
@@ -0,0 +1,30 @@
+package baseDatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Create {
+
+    public static void createTable() {
+
+        String url = "jdbc:sqlite:puntuacion.db";
+
+        String sql = "CREATE TABLE IF NOT EXISTS puntuacion ("
+                + "	name text NOT NULL,"
+                + "	puntos text NOT NULL,"
+                  + "	id integer PRIMARY KEY"
+                + ");";
+
+        try (Connection conn = DriverManager.getConnection(url);
+                Statement stmt = conn.createStatement()) {
+
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+}
